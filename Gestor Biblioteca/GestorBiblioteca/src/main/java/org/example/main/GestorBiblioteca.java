e36102d4b9b30811bf70750153a9b9c15599b480
José Vicente Sánchez Vargues <josevi97sgb@gmail.com>
2026-05-31 14:04:17 +0200

    Agregar método para comprobar que no se repitan IDs de las copias.

--- Gestor Biblioteca/GestorBiblioteca/src/main/java/org/example/main/GestorBiblioteca.java
+++ Gestor Biblioteca/GestorBiblioteca/src/main/java/org/example/main/GestorBiblioteca.java
@@ -52,35 +52,48 @@ public class GestorBiblioteca
         System.out.println("11.- Salir.");
     }
 
+    public static boolean existeCopia(int id){
+        ArrayList<Copia> copias = copiaDAOImpl.getAll();
+
+        for (Copia copia : copias){
+            if(copia.getId() == id){
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void crearCopia(){
         System.out.println("Ingrese id de la copia.");
         int id = sc.nextInt();
         sc.nextLine();
 
-        //comprobar id
+        if (!existeCopia(id)){
+            System.out.println("Ingrese nombre de la copia.");
+            String nombre = sc.nextLine();
 
-        System.out.println("Ingrese nombre de la copia.");
-        String nombre = sc.nextLine();
+            System.out.println("Ingrese el tipo de la copia");
+            String tipo = sc.nextLine();
 
-        System.out.println("Ingrese el tipo de la copia");
-        String tipo = sc.nextLine();
+            System.out.println("Ingrese la editorial de la copia.");
+            String editorial = sc.nextLine();
 
-        System.out.println("Ingrese la editorial de la copia.");
-        String editorial = sc.nextLine();
+            System.out.println("Ingrese el año de la copia.");
+            int ano =  sc.nextInt();
+            sc.nextLine();
 
-        System.out.println("Ingrese el año de la copia.");
-        int ano =  sc.nextInt();
-        sc.nextLine();
+            System.out.println("Ingrese el autor de la copia.");
+            String autor = sc.nextLine();
 
-        System.out.println("Ingrese el autor de la copia.");
-        String autor = sc.nextLine();
+            System.out.println("Ingrese el estado de la copia.");
+            String estado = sc.nextLine();
 
-        System.out.println("Ingrese el estado de la copia.");
-        String estado = sc.nextLine();
+            Copia copia = new Copia(id, nombre, tipo, editorial, ano, autor, estado);
 
-        Copia copia = new Copia(id, nombre, tipo, editorial, ano, autor, estado);
-
-        copiaDAOImpl.add(copia);
+            copiaDAOImpl.add(copia);
+        } else {
+            System.out.println("Ya existe una copia con esa ID. Volviendo al menú principal.");
+        }
     }
 
     public static void listarCopias(){
